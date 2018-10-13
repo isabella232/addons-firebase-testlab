cedcf95a4a369abcb5150ecb64c639badc4f8062
erosdome <jozsef.eros@bitrise.io>
2018-10-13 10:05:47 +0200

    Parse iOS logs too (#6)

--- frontend/assets/compiled/javascripts/Logs.js
+++ frontend/assets/compiled/javascripts/Logs.js
@@ -44,11 +44,24 @@ angular.module("BitriseAddonFirebaseApp").controller("LogsController", function(
 		unwatchTest();
 	});
 
+	var projectTypes = [{
+		isLogDataWithProjectType: detectorMethodForAndroid,
+		logProcessMethodForProjectType: logProcessMethodForAndroid
+	}, {
+		isLogDataWithProjectType: detectorMethodForIos,
+		logProcessMethodForProjectType: logProcessMethodForIos
+	}];
+	var currentProjectType;
+
 	function loadLogs() {
 		viewModel.loadLogsProgress.start("Loading logs, wait a sec...");
 
 		requestService.getLogfromURL(pageDetailsService.test.logsURL).then(function(data) {
-			pageDetailsService.logs = processedLogs(data);
+			currentProjectType = _.find(projectTypes, function(aProjectType) {
+				return aProjectType.isLogDataWithProjectType(data);
+			});
+
+			pageDetailsService.logs = currentProjectType.logProcessMethodForProjectType(data);
 			viewModel.pagination.selectedPage = 1;
 
 			viewModel.loadLogsProgress.success();
@@ -61,7 +74,35 @@ angular.module("BitriseAddonFirebaseApp").controller("LogsController", function(
 		});
 	}
 
-	function processedLogs(rawLogs) {
+	function detectorMethodForAndroid(rawLogs) {
+		return _.any(_.filter(rawLogs.split("\n"), function(aRawLogLine) {
+			return aRawLogLine;
+		}), function(aRawLogLine) {
+			if (!aRawLogLine) {
+				return false;
+			}
+
+			var regexp = new RegExp(/^([0-9]+)-([0-9]+) ([0-9]+)\:([0-9]+)\:([0-9]+)\.([0-9]+)\: (?:[0-9]+\-[0-9]+\/.+ |)([V|D|I|W|E|A])\/(.+)\([0-9]+\)\: (.+)$/);
+
+			return regexp.test(aRawLogLine);
+		});
+	}
+
+	function detectorMethodForIos(rawLogs) {
+		return _.any(_.filter(rawLogs.split("\n"), function(aRawLogLine) {
+			return aRawLogLine;
+		}), function(aRawLogLine) {
+			if (!aRawLogLine) {
+				return false;
+			}
+
+			var regexp = new RegExp(/^(.*)  ([0-9]{0,2}) ([0-9]{0,2}):([0-9]{0,2}):([0-9]{0,2}) ([^\[]*)\[[0-9]*] <([^>]*)>: (.*)$/);
+
+			return regexp.test(aRawLogLine);
+		});
+	}
+
+	function logProcessMethodForAndroid(rawLogs) {
 		return _.map(_.filter(rawLogs.split("\n"), function(aRawLogLine) {
 			return aRawLogLine;
 		}), function(aRawLogLine) {
@@ -165,6 +206,114 @@ angular.module("BitriseAddonFirebaseApp").controller("LogsController", function(
 		});
 	}
 
+	function logProcessMethodForIos(rawLogs) {
+		var logLines = [];
+		var currentLogLine;
+
+		_.each(_.filter(rawLogs.split("\n"), function(aRawLogLine) {
+			return aRawLogLine;
+		}), function(aRawLogLine) {
+			if (!aRawLogLine) {
+				return null;
+			}
+
+			try {
+				var regexp = new RegExp(/^(.*)  ([0-9]{0,2}) ([0-9]{0,2}):([0-9]{0,2}):([0-9]{0,2}) ([^\[]*)\[[0-9]*] <([^>]*)>: (.*)$/);
+
+				if (!regexp.test(aRawLogLine)) {
+					if (currentLogLine) {
+						currentLogLine.message += "\n" + aRawLogLine;
+					}
+					else {
+						logLines.push({
+							date: undefined,
+							type: {
+								id: "verbose",
+								cssClass: "verbose"
+							},
+							tag: undefined,
+							message: aRawLogLine,
+							isProcessed: true,
+							isExpanded: false
+						});
+					}
+
+					return;
+				}
+
+				var month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"].indexOf(regexp.exec(aRawLogLine)[1]);
+				var day = regexp.exec(aRawLogLine)[2];
+				var hour = regexp.exec(aRawLogLine)[3];
+				var minute = regexp.exec(aRawLogLine)[4];
+				var second = regexp.exec(aRawLogLine)[5];
+				var tag = regexp.exec(aRawLogLine)[6];
+				var type;
+
+				switch (regexp.exec(aRawLogLine)[7]) {
+					case "Notice":
+						type = {
+							id: "info",
+							cssClass: "info"
+						};
+
+						break;
+					case "Error":
+						type = {
+							id: "error",
+							cssClass: "error"
+						};
+
+						break;
+					default:
+						console.log(aRawLogLine);
+
+						break;
+				}
+
+				var message = regexp.exec(aRawLogLine)[8];
+
+				var date = new Date(new Date().getFullYear(), month, day, hour, minute, second);
+
+				currentLogLine = {
+					date: date,
+					type: type,
+					tag: tag,
+					message: message,
+					isProcessed: true,
+					isExpanded: false
+				};
+
+				logLines.push(currentLogLine);
+			}
+			catch (error) {
+				var type = {
+					id: "verbose",
+					cssClass: "verbose"
+				};
+
+				viewModel.selectedLogTypeFilter = _.find(viewModel.logTypeFilters, {
+					id: "all"
+				});
+
+				viewModel.logTypeFilters = [{
+					id: "all",
+					name: "All logs"
+				}]
+
+				logLines.push({
+					date: undefined,
+					type: type,
+					tag: undefined,
+					message: aRawLogLine,
+					isProcessed: true,
+					isExpanded: false
+				});
+			}
+		});
+
+		return logLines;
+	}
+
 	viewModel.logTypeFilterSelected = function(logTypeFilter) {
 		viewModel.selectedLogTypeFilter = logTypeFilter;
 		viewModel.isLogTypeFilterVisible = false;
@@ -182,7 +331,7 @@ angular.module("BitriseAddonFirebaseApp").controller("LogsController", function(
 
 			switch (viewModel.selectedLogTypeFilter.id) {
 				case "all":
-					return _.contains(["verbose", "debug", "info", "warning", "error", "assert"], aLogLine.type.id);
+					return _.contains(["verbose", "debug", "info", "warning", "error", "assert", "notice"], aLogLine.type.id);
 				case "info+":
 					return _.contains(["info", "warning", "error", "assert"], aLogLine.type.id);
 				case "warning+":
