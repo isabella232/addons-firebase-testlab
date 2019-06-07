dbbd0f21a443f71304c145f8a303b9e0ae115226
lszucs <44788749+lszucs@users.noreply.github.com>
2019-06-07 10:24:41 +0200

    fix: 45 min limit error (#55)

--- actions/test.go
+++ actions/test.go
@@ -2,7 +2,9 @@ package actions
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
 
@@ -241,6 +243,16 @@ func TestPost(c buffalo.Context) error {
 	if postTestrequestModel.EnvironmentMatrix.IosDeviceList != nil {
 		if err := firebaseutils.ValidateIosDevices(postTestrequestModel.EnvironmentMatrix.IosDeviceList.IosDevices); err != nil {
 			return c.Render(http.StatusNotAcceptable, r.String("Invalid device configuration: %s", err))
+		}
+	}
+
+	if timeout := postTestrequestModel.TestSpecification.TestTimeout; timeout != "" {
+		secs, err := strconv.ParseFloat(strings.TrimSuffix(timeout, "s"), 32)
+		if err == nil {
+			if secs > 2700.0 {
+				logger.Warn(fmt.Sprintf("Incoming TestSpecification.TestTimeout '%s' from build '%s' exceeds limit of '2700s', overriding it to '2700s'", timeout, appSlug))
+				postTestrequestModel.TestSpecification.TestTimeout = "2700s"
+			}
 		}
 	}
 
