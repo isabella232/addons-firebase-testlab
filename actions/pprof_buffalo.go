0c7921654b2c2c036ed1a0c724c59ccb68893e18
Adam Pelle <adam.pelle@bitrise.io>
2020-04-28 13:41:31 +0200

    [NOJIRA] added pprof endpoints (#81)
    * added pprof endpoints
    
    * fix linting

---
+++ actions/pprof_buffalo.go
@@ -0,0 +1,31 @@
+package actions
+
+import (
+	"net/http/pprof"
+
+	"github.com/gobuffalo/buffalo"
+)
+
+// RegisterProfEndpoints Registers pprof endpoints
+func RegisterProfEndpoints(app *buffalo.App) {
+	profiles := []string{
+		"allocs",
+		"block",
+		"heap",
+		"mutex",
+		"threadcreate",
+		"trace",
+		"goroutine",
+	}
+
+	for _, profile := range profiles {
+		app.GET("/debug_pprof_prof/"+profile, profEndpoint(profile))
+	}
+}
+
+func profEndpoint(name string) buffalo.Handler {
+	return func(c buffalo.Context) error {
+		pprof.Handler(name).ServeHTTP(c.Response(), c.Request())
+		return nil
+	}
+}
